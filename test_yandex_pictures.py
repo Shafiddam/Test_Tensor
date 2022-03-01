d1e790b9095c314b8dba1accf74d92c0d0e47fbc
Фидаиль <93327801+Shafiddam@users.noreply.github.com>
2022-03-01 12:58:48 +0300

    доработал код, это финал)

--- test_yandex_pictures.py
+++ test_yandex_pictures.py
@@ -1,5 +1,4 @@
-from .pages.base_page import BasePage
-import pytest
+from pages.base_page import BasePage
 
 
 def test_yandex_pictures(browser):
@@ -9,6 +8,8 @@ def test_yandex_pictures(browser):
     2)	Ссылка «Картинки» присутствует на странице
     3)	Кликаем на ссылку
     4)	Проверить, что перешли на url https://yandex.ru/images/
+
+
     5)	Открыть 1 категорию, проверить что открылась, в поиске верный текст
     6) Открыть 1 картинку , проверить что открылась
     7) При нажатии кнопки вперед  картинка изменяется
@@ -18,5 +19,10 @@ def test_yandex_pictures(browser):
     link = 'http://yandex.ru'
     page = BasePage(browser, link)
     page.open()
-    # page.go_to_login_mail_page()
-    # page.find_count_mail_and_write_mail()
+    page.should_be_link_images_presented()
+    page.should_be_current_link_is_yandex_images(browser)
+    page.open_first_category()
+    page.open_first_image(browser)
+    page.open_next_image()
+    page.open_prev_image()
+    page.should_be_first_images(browser)
