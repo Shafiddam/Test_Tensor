e9d3c11af6b407d95c8f8ee40a3b60be3d3a7b92
Фидаиль <93327801+Shafiddam@users.noreply.github.com>
2022-03-01 12:58:03 +0300

    добавил локаторов

--- pages/locators.py
+++ pages/locators.py
@@ -12,4 +12,10 @@ class BasePageLocators():
     DATA_CID_4 = (By.CSS_SELECTOR, 'li[data-cid="4"] a[href^="https://tensor.ru"]')
     DATA_CID_5 = (By.CSS_SELECTOR, 'li[data-cid="5"] a[href^="https://tensor.ru"]')
 
-
+    LINK_SEARCH_IMAGES = (By.CSS_SELECTOR, 'a[href^="https://yandex.ru/images"')
+    LINK_CATEGORY_0 = (By.CSS_SELECTOR, 'div.PopularRequestList-Item.PopularRequestList-Item_pos_0')
+    TEXT_CATEGORY_0 = (By.CSS_SELECTOR, 'div.PopularRequestList-SearchText')
+    TEXT_SEARCH_0 = (By.CSS_SELECTOR, 'head title')
+    LINK_IMAGE_0 = (By.CSS_SELECTOR, '.serp-item_pos_0')
+    CIRCLEBUTTON_TYPE_NEXT = (By.CSS_SELECTOR, '.CircleButton_type_next')
+    CIRCLEBUTTON_TYPE_PREV = (By.CSS_SELECTOR, '.CircleButton_type_prev')
